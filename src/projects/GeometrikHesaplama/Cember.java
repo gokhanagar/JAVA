c7cf5d921c7cbfc5beb5295a8592138ca0c2755f
Gokhan Agar <gokhanagar1@gmail.com>
2022-06-14 16:35:50 +0300

    2022-06-14

---
+++ src/projects/GeometrikHesaplama/Cember.java
@@ -0,0 +1,26 @@
+package projects.GeometrikHesaplama;
+
+public class Cember extends Sekil {
+
+    public Cember(double yaricap,double pi,double etkisiz) {
+        super(yaricap,pi,etkisiz);
+    }
+
+    @Override
+    public double alanHesaplama() {
+        return yaricap*yaricap*pi;
+    }
+
+    @Override
+    public double cevreHesaplama() {
+        return 2*pi*yaricap;
+    }
+
+    @Override
+    public String toString() {
+        return "{ yaricap = " + yaricap + " cember alan " + alanHesaplama() + " cember cevre " + cevreHesaplama() +
+                '}';
+    }
+
+
+}
